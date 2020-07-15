c5d0d091f02d246e5db5d007913406e8d3ec79f6
Gianluca Maccioni <gianluca.maccioni.uk@gmail.com>
2020-07-15 11:14:37 +0100

    Remove unused dependencies

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const urlExists = require('url-exists');
